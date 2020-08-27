cf057e981d6f5c5a67d506b3c126a42d8f77c4ab
Enzias <68203437+Enzias@users.noreply.github.com>
2020-08-27 15:57:26 +0200

    Memory Use Improvements

--- src/fr/enzias/easyduels/managers/TimerManager.java
+++ src/fr/enzias/easyduels/managers/TimerManager.java
@@ -41,7 +41,7 @@ public class TimerManager extends BukkitRunnable {
 
                 if (settings.getLobbyMessageToPlayers(lobbyTime) != null)
                     sender.sendMessage(settings.getLobbyMessageToPlayers(lobbyTime)
-                                    .replaceAll("%player1%", arena.getFirstPlayer().getName()).replaceAll("%player2%", arena.getSecondPlayer().getName()),
+                                    .replaceAll("%player1%", arena.getFirstPlayerName()).replaceAll("%player2%", arena.getSecondPlayerName()),
                             arena.getFirstPlayer(), arena.getSecondPlayer());
                 if (settings.getLobbyTitleToPlayers(lobbyTime) != null)
                     sender.sendTitle(settings.getLobbyTitleToPlayers(lobbyTime), 1, 18, 1,
@@ -51,22 +51,22 @@ public class TimerManager extends BukkitRunnable {
                             arena.getFirstPlayer(), arena.getSecondPlayer());
                 if (settings.getLobbyActionbarToPlayers(lobbyTime) != null)
                     sender.sendActionbar(settings.getLobbyActionbarToPlayers(lobbyTime)
-                                    .replaceAll("%player1%", arena.getFirstPlayer().getName()).replaceAll("%player2%", arena.getSecondPlayer().getName()),
+                                    .replaceAll("%player1%", arena.getFirstPlayerName()).replaceAll("%player2%", arena.getSecondPlayerName()),
                             1, 18, 1, arena.getFirstPlayer(), arena.getSecondPlayer());
                 if (settings.getLobbyPlayersCommand(lobbyTime) != null) {
                     sender.sendPlayerCommand(settings.getLobbyPlayersCommand(lobbyTime)
-                                    .replaceAll("%player%", arena.getFirstPlayer().getName()).replaceAll("%opponent%", arena.getSecondPlayer().getName()),
+                                    .replaceAll("%player%", arena.getFirstPlayerName()).replaceAll("%opponent%", arena.getSecondPlayerName()),
                             arena.getFirstPlayer());
                     sender.sendPlayerCommand(settings.getLobbyPlayersCommand(lobbyTime)
-                                    .replaceAll("%player%", arena.getSecondPlayer().getName()).replaceAll("%opponent%", arena.getFirstPlayer().getName()),
+                                    .replaceAll("%player%", arena.getSecondPlayerName()).replaceAll("%opponent%", arena.getFirstPlayerName()),
                             arena.getSecondPlayer());
                 }
                 if (settings.getLobbyConsoleCommand(lobbyTime) != null)
                     sender.sendConsoleCommand(settings.getLobbyConsoleCommand(lobbyTime)
-                            .replaceAll("%player1%", arena.getFirstPlayer().getName()).replaceAll("%player2%", arena.getSecondPlayer().getName()), settings.getSyncTimer());
+                            .replaceAll("%player1%", arena.getFirstPlayerName()).replaceAll("%player2%", arena.getSecondPlayerName()), settings.getSyncTimer());
                 if (settings.getLobbyBroadcast(lobbyTime) != null)
                     sender.sendConsoleCommand(settings.getLobbyBroadcast(lobbyTime)
-                            .replaceAll("%player1%", arena.getFirstPlayer().getName()).replaceAll("%player2%", arena.getSecondPlayer().getName()), settings.getSyncTimer());
+                            .replaceAll("%player1%", arena.getFirstPlayerName()).replaceAll("%player2%", arena.getSecondPlayerName()), settings.getSyncTimer());
 
             }
 
@@ -83,7 +83,7 @@ public class TimerManager extends BukkitRunnable {
 
                 if (settings.getFightMessageToPlayers(fightTime) != null)
                     sender.sendMessage(settings.getFightMessageToPlayers(fightTime)
-                                    .replaceAll("%player1%", arena.getFirstPlayer().getName()).replaceAll("%player2%", arena.getSecondPlayer().getName()),
+                                    .replaceAll("%player1%", arena.getFirstPlayerName()).replaceAll("%player2%", arena.getSecondPlayerName()),
                             arena.getFirstPlayer(), arena.getSecondPlayer());
                 if (settings.getFightTitleToPlayers(fightTime) != null)
                     sender.sendTitle(settings.getFightTitleToPlayers(fightTime), 1, 18, 1,
@@ -93,19 +93,19 @@ public class TimerManager extends BukkitRunnable {
                             arena.getFirstPlayer(), arena.getSecondPlayer());
                 if (settings.getFightPlayersCommand(fightTime) != null){
                     sender.sendPlayerCommand(settings.getFightPlayersCommand(lobbyTime)
-                                    .replaceAll("%player%", arena.getFirstPlayer().getName()).replaceAll("%opponent%", arena.getSecondPlayer().getName()),
+                                    .replaceAll("%player%", arena.getFirstPlayerName()).replaceAll("%opponent%", arena.getSecondPlayerName()),
                             arena.getFirstPlayer());
                     sender.sendPlayerCommand(settings.getFightPlayersCommand(lobbyTime)
-                                    .replaceAll("%player%", arena.getSecondPlayer().getName()).replaceAll("%opponent%", arena.getFirstPlayer().getName()),
+                                    .replaceAll("%player%", arena.getSecondPlayerName()).replaceAll("%opponent%", arena.getFirstPlayerName()),
                             arena.getSecondPlayer());
                 }
                 if (settings.getFightConsoleCommand(fightTime) != null)
                     sender.sendConsoleCommand(settings.getLobbyConsoleCommand(fightTime)
-                            .replaceAll("%player1%", arena.getFirstPlayer().getName()).replaceAll("%player2%", arena.getSecondPlayer().getName()),
+                            .replaceAll("%player1%", arena.getFirstPlayerName()).replaceAll("%player2%", arena.getSecondPlayerName()),
                             settings.getSyncTimer());
                 if (settings.getFightBroadcast(fightTime) != null)
                     sender.sendConsoleCommand(settings.getLobbyBroadcast(fightTime)
-                            .replaceAll("%player1%", arena.getFirstPlayer().getName()).replaceAll("%player2%", arena.getSecondPlayer().getName()),
+                            .replaceAll("%player1%", arena.getFirstPlayerName()).replaceAll("%player2%", arena.getSecondPlayerName()),
                             settings.getSyncTimer());
 
             }
@@ -113,7 +113,7 @@ public class TimerManager extends BukkitRunnable {
                 if(settings.getFightAllActionbarToPlayers() != null)
                     sender.sendActionbar(settings.getFightAllActionbarToPlayers()
                                     .replaceAll("%timer%", count.secondToString(fightTime))
-                                    .replaceAll("%player1%", arena.getFirstPlayer().getName()).replaceAll("%player2%", arena.getSecondPlayer().getName()),
+                                    .replaceAll("%player1%", arena.getFirstPlayerName()).replaceAll("%player2%", arena.getSecondPlayerName()),
                             1, 18, 1, arena.getFirstPlayer(), arena.getSecondPlayer());
 
             if(fightTime <= 0){
@@ -126,13 +126,12 @@ public class TimerManager extends BukkitRunnable {
         if(arena.isStatut(ArenaStatuts.RELOADING)){
 
             if(arena.getFirstPlayer().isOnline())
-                sender.sendGameMode(settings.getEndGameMode()
-                                .replaceAll("%player1%", arena.getFirstPlayer().getName()).replaceAll("%player2%", arena.getSecondPlayer().getName()),
-                        settings.getSyncTimer(), arena.getFirstPlayer());
+                sender.sendGameMode(settings.getEndGameMode(), settings.getSyncTimer(), arena.getFirstPlayer());
             if(arena.getSecondPlayer().isOnline())
                 sender.sendGameMode(settings.getEndGameMode(), settings.getSyncTimer(), arena.getSecondPlayer());
+
             if(reloadTime == settings.getEndTime()) {
-                if (arena.getWinner() == null) { //no winner
+                if (arena.getWinner() == null) {
                     if (settings.getResultMessage())
                         sender.sendMessage(message.getNoWinner(), arena.getFirstPlayer(), arena.getSecondPlayer());
                     if(settings.getEndSoundNoWinner() != null)
@@ -143,66 +142,41 @@ public class TimerManager extends BukkitRunnable {
                         sender.sendActionbar(settings.getEndActionbarDraw(), 20, settings.getEndTime()*20, 20, arena.getFirstPlayer(), arena.getSecondPlayer());
                     if(settings.getEndBroadcastNoWinner() != null)
                         sender.sendBroadcast(settings.getEndBroadcastNoWinner()
-                                .replaceAll("%player%", arena.getFirstPlayer().getName()).replaceAll("%player2%", arena.getSecondPlayer().getName()), settings.getSyncTimer());
+                                .replaceAll("%player%", arena.getFirstPlayerName()).replaceAll("%player2%", arena.getSecondPlayerName()), settings.getSyncTimer());
 
-                } else { //a winner
+                } else {
 
-                    //getWinner
                     if (settings.getFakeExplosion())
                         sender.sendExplosion(arena.getWinner());
                     if (settings.getFirework())
                         sender.sendFirework(arena.getWinner(), settings.getSyncTimer());
 
-                    if (arena.getWinner().getUniqueId().equals(arena.getSecondPlayerUUID())) { //first is the loser
-                        if (settings.getResultMessage()) { //result message
-                            sender.sendMessage(message.getWinner().replaceAll("%winner%", arena.getWinner().getName()).replaceAll("%loser%", arena.getFirstPlayer().getName()), arena.getWinner());
-                            if (arena.getFirstPlayer().isOnline())
-                                sender.sendMessage(message.getLoser().replaceAll("%winner%", arena.getWinner().getName()).replaceAll("%loser%", arena.getFirstPlayer().getName()), arena.getFirstPlayer());
-                        }
-                        if (settings.getEndTitleToWinner() != null)
-                            sender.sendTitle(settings.getEndTitleToWinner(), 20, settings.getEndTime() * 20, 20, arena.getWinner());
-                        if (settings.getEndTitleToLoser() != null && arena.getFirstPlayer().isOnline())
-                            sender.sendTitle(settings.getEndTitleToLoser(), 20, settings.getEndTime() * 20, 20, arena.getFirstPlayer());
-                        if(settings.getEndSoundToWinner() != null)
-                            sender.sendSound(settings.getEndSoundToWinner(), settings.getEndVolumeToWinner(), settings.getEndPitchToWinner(), arena.getWinner());
-                        if(settings.getEndSoundToLoser() != null && arena.getFirstPlayer().isOnline())
-                            sender.sendSound(settings.getEndSoundToLoser(), settings.getEndVolumeToLoser(), settings.getEndPitchToLoser(), arena.getFirstPlayer());
-                        if(settings.getEndActionbarToWinner() != null)
-                            sender.sendActionbar(settings.getEndActionbarToWinner(), 20, settings.getEndTime()*20, 20, arena.getWinner());
-                        if(settings.getEndActionbarToLoser() != null && arena.getFirstPlayer().isOnline())
-                            sender.sendActionbar(settings.getEndActionbarToLoser(), 20, settings.getEndTime()*20, 20, arena.getSecondPlayer());
-                        if(settings.getEndRewardCommand() != null)
-                            sender.sendConsoleCommand(settings.getEndRewardCommand(), settings.getSyncTimer(),
-                                    "%winner%", arena.getWinner().getName(), "%loser%", arena.getFirstPlayer().getName());
-                        if(settings.getEndBroadcast() != null)
-                            sender.sendBroadcast(settings.getEndBroadcast()
-                                    .replaceAll("%winner%", arena.getWinner().getName()).replaceAll("%loser%", arena.getFirstPlayer().getName()), settings.getSyncTimer());
-                    }else { //second is the loser
-
-                        if (settings.getResultMessage()) { //result message
-                            sender.sendMessage(message.getWinner().replaceAll("%winner%", arena.getWinner().getName()).replaceAll("%loser%", arena.getSecondPlayer().getName()), arena.getWinner());
-                            if (arena.getSecondPlayer().isOnline())
-                                sender.sendMessage(message.getLoser().replaceAll("%winner%", arena.getWinner().getName()).replaceAll("%loser%", arena.getSecondPlayer().getName()), arena.getSecondPlayer());
-                        }
-                        if (settings.getEndTitleToWinner() != null)
-                            sender.sendTitle(settings.getEndTitleToWinner(), 20, settings.getEndTime() * 20, 20, arena.getWinner());
-                        if (settings.getEndTitleToLoser() != null && arena.getSecondPlayer().isOnline())
-                            sender.sendTitle(settings.getEndTitleToLoser(), 20, settings.getEndTime() * 20, 20, arena.getSecondPlayer());
-                        if (settings.getEndSoundToWinner() != null)
-                            sender.sendSound(settings.getEndSoundToWinner(), settings.getEndVolumeToWinner(), settings.getEndPitchToWinner(), arena.getWinner());
-                        if (settings.getEndSoundToLoser() != null && arena.getSecondPlayer().isOnline())
-                            sender.sendSound(settings.getEndSoundToLoser(), settings.getEndVolumeToLoser(), settings.getEndPitchToLoser(), arena.getSecondPlayer());
-                        if (settings.getEndActionbarToWinner() != null)
-                            sender.sendActionbar(settings.getEndActionbarToWinner(), 20, settings.getEndTime() * 20, 20, arena.getWinner());
-                        if (settings.getEndActionbarToLoser() != null && arena.getSecondPlayer().isOnline())
-                            sender.sendActionbar(settings.getEndActionbarToLoser(), 20, settings.getEndTime() * 20, 20, arena.getSecondPlayer());
-                        if(settings.getEndRewardCommand() != null)
-                            sender.sendConsoleCommand(settings.getEndRewardCommand(), settings.getSyncTimer(),
-                                    "%winner%", arena.getWinner().getName(), "%loser%", arena.getSecondPlayer().getName());
-                        if(settings.getEndBroadcast() != null)
-                            sender.sendBroadcast(settings.getEndBroadcast()
-                                    .replaceAll("%winner%", arena.getWinner().getName()).replaceAll("%loser%", arena.getSecondPlayer().getName()), settings.getSyncTimer());
+                    if (settings.getResultMessage()) { //result message
+                        sender.sendMessage(message.getWinner().replaceAll("%winner%", arena.getWinnerName()).replaceAll("%loser%", arena.getLoserName()), arena.getWinner());
+                        if (arena.getLoser().isOnline())
+                            sender.sendMessage(message.getLoser().replaceAll("%winner%", arena.getWinnerName()).replaceAll("%loser%", arena.getLoserName()), arena.getLoser());
                     }
+
+                    if(arena.getLoser().isOnline()){
+                        if (settings.getEndTitleToLoser() != null)
+                            sender.sendTitle(settings.getEndTitleToLoser(), 20, settings.getEndTime() * 20, 20, arena.getLoser());
+                        if (settings.getEndSoundToLoser() != null)
+                            sender.sendSound(settings.getEndSoundToLoser(), settings.getEndVolumeToLoser(), settings.getEndPitchToLoser(), arena.getLoser());
+                        if (settings.getEndActionbarToLoser() != null)
+                            sender.sendActionbar(settings.getEndActionbarToLoser(), 20, settings.getEndTime() * 20, 20, arena.getLoser());
+                    }
+                    if (settings.getEndTitleToWinner() != null)
+                        sender.sendTitle(settings.getEndTitleToWinner(), 20, settings.getEndTime() * 20, 20, arena.getWinner());
+                    if (settings.getEndSoundToWinner() != null)
+                        sender.sendSound(settings.getEndSoundToWinner(), settings.getEndVolumeToWinner(), settings.getEndPitchToWinner(), arena.getWinner());
+                    if (settings.getEndActionbarToWinner() != null)
+                        sender.sendActionbar(settings.getEndActionbarToWinner(), 20, settings.getEndTime() * 20, 20, arena.getWinner());
+                    if (settings.getEndRewardCommand() != null)
+                        sender.sendConsoleCommand(settings.getEndRewardCommand(), settings.getSyncTimer(),
+                                "%winner%", arena.getWinnerName(), "%loser%", arena.getLoserName());
+                    if (settings.getEndBroadcast() != null)
+                        sender.sendBroadcast(settings.getEndBroadcast()
+                                .replaceAll("%winner%", arena.getWinnerName()).replaceAll("%loser%", arena.getLoserName()), settings.getSyncTimer());
                 }
             }
             if(reloadTime <= 0) {
