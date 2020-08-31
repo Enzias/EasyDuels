4f0e78caa93c424000443ace11089e823dc069b8
Enzias <68203437+Enzias@users.noreply.github.com>
2020-08-31 19:28:38 +0200

    Added queue check

--- src/fr/enzias/easyduels/listeners/LeaveEvent.java
+++ src/fr/enzias/easyduels/listeners/LeaveEvent.java
@@ -7,6 +7,7 @@ import fr.enzias.easyduels.arena.ArenaStatuts;
 import fr.enzias.easyduels.files.ArenaFile;
 import fr.enzias.easyduels.files.SettingsFile;
 import fr.enzias.easyduels.managers.RequestManager;
+import fr.enzias.easyduels.queue.QueueManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,13 +20,14 @@ public class LeaveEvent implements Listener {
     ArenaFile arenaFile;
     Arena arena;
     RequestManager request;
+    QueueManager queue;
     public LeaveEvent(EasyDuels plugin) {
         this.plugin = plugin;
         this.settings = plugin.getSettingsFile();
         this.arenaFile = plugin.getArenaFile();
         this.arena = plugin.getArena();
         this.request = plugin.getRequest();
-
+        this.queue = plugin.getQueue();
     }
 
     @EventHandler
@@ -34,6 +36,15 @@ public class LeaveEvent implements Listener {
 
         if (request.hasRequest(player))
             request.deleteRequest(player);
+
+        if(queue.isInQueue(player)){
+            queue.deleteQueue(player);
+        }
+        if(queue.isAnOpponent(player)){
+            if(queue.isInQueue(queue.getOpponentOf(player)))
+                queue.deleteQueue(queue.getOpponentOf(player));
+        }
+
         if (arena.isStatut(ArenaStatuts.LOBBY) || arena.isStatut(ArenaStatuts.PLAYING)) {
             if (player.getLocation().getWorld().getName().equalsIgnoreCase(arenaFile.getWorldName())
                     && arena.getPlayers().contains(player)) {
