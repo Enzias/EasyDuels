6ce68890767172dd94b3076c72d1f572310d0d87
Enzias <68203437+Enzias@users.noreply.github.com>
2020-09-01 22:14:09 +0200

    Added New admin command

--- src/fr/enzias/easyduels/commands/SubCommands/AdminCommand/AdminCommand.java
+++ src/fr/enzias/easyduels/commands/SubCommands/AdminCommand/AdminCommand.java
@@ -1,10 +1,15 @@
 package fr.enzias.easyduels.commands.SubCommands.AdminCommand;
 
 import fr.enzias.easyduels.EasyDuels;
+import fr.enzias.easyduels.arena.Arena;
+import fr.enzias.easyduels.arena.ArenaStatuts;
 import fr.enzias.easyduels.commands.SubCommand;
 import fr.enzias.easyduels.commands.SubCommands.AdminCommand.SubCommands.*;
 import fr.enzias.easyduels.files.MessageFile;
+import fr.enzias.easyduels.files.SettingsFile;
 import fr.enzias.easyduels.managers.SenderManager;
+import fr.enzias.easyduels.queue.QueueManager;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -12,12 +17,19 @@ import java.util.ArrayList;
 public class AdminCommand extends SubCommand {
 
     private ArrayList<SubCommand> commands = new ArrayList<>();
+    Arena arena;
     MessageFile messageFile;
+    SettingsFile settingsFile;
     SenderManager sender;
+    QueueManager queue;
+
     public AdminCommand(EasyDuels plugin) {
         super(plugin);
+        this.arena = plugin.getArena();
         this.messageFile = plugin.getMessageFile();
+        this.settingsFile = plugin.getSettingsFile();
         this.sender = plugin.getSender();
+        this.queue = plugin.getQueue();
         this.commands.add(new FirstSpawnCommand(plugin));
         this.commands.add(new SecondSpawnCommand(plugin));
         this.commands.add(new HelpCommand(plugin));
@@ -31,19 +43,58 @@ public class AdminCommand extends SubCommand {
 
         if (player.hasPermission("easyduels.admin")) {
 
-            if(args.length >= 2) {
+            if (args.length >= 2) {
                 for (int i = 0; i < getSubCommands().size(); i++)
                     if (args[1].equalsIgnoreCase(getSubCommands().get(i).getName())) {
                         getSubCommands().get(i).onCommand(player, args);
                         return;
                     }
             }
+            if (args.length == 3) {
+                if (Bukkit.getPlayer(args[1]) == null) {
+                    sender.sendMessage(messageFile.getOfflinePlayer().replaceAll("%player%", args[1]));
+                    return;
+                }
+                if (Bukkit.getPlayer(args[2]) == null) {
+                    sender.sendMessage(messageFile.getOfflinePlayer().replaceAll("%player%", args[2]));
+                    return;
+                }
+                Player player1 = Bukkit.getPlayer(args[1]);
+                Player player2 = Bukkit.getPlayer(args[2]);
 
-            sender.sendMessage(messageFile.getAdminUnknown(), player);
+                if (!arena.isStatut(ArenaStatuts.IDLE) && arena.getPlayers().contains(player1)) {
+                    sender.sendMessage(messageFile.getAdminJoinQueueInDuel().replaceAll("%player%", player1.getName()), player);
+                    return;
+                }
+                if (!arena.isStatut(ArenaStatuts.IDLE) && arena.getPlayers().contains(player2)) {
+                    sender.sendMessage(messageFile.getAdminJoinQueueInDuel().replaceAll("%player%", player2.getName()), player);
+                    return;
+                }
 
-        } else {
+                if (settingsFile.getQueue()) {
+                    if (queue.isNotFull()) {
+                        queue.addQueueLast(player1, player2);
+                        queue.checkQueue();
+                    } else
+                        sender.sendMessage(messageFile.getQueueIsFull(), player);
+                } else {
+                    if (!arena.isStatut(ArenaStatuts.IDLE))
+                        sender.sendMessage(messageFile.getArenaNotEmpty(), player);
+                    else {
+
+                        arena.addToArena(player1, player2);
+                        arena.setLastLocation(player1, player2);
+                        arena.teleportToLocation(player1, player2, settingsFile.getSyncTimer());
+                        arena.startMatch();
+
+                    }
+                }
+
+            } else {
+                sender.sendMessage(messageFile.getAdminUnknown(), player);
+            }
+        } else
             sender.sendMessage(messageFile.getNoPermission(), player);
-        }
     }
 
     @Override
@@ -51,7 +102,7 @@ public class AdminCommand extends SubCommand {
         return "admin";
     }
 
-    public ArrayList<SubCommand> getSubCommands(){
+    public ArrayList<SubCommand> getSubCommands() {
         return commands;
     }
 }
