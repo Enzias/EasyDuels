99e12ac5a94a6955988ed3196a43da60f844f101
Enzias <68203437+Enzias@users.noreply.github.com>
2020-09-08 16:04:41 +0200

    Timer Manager update

---
+++ src/fr/enzias/easyduels/tasks/TimerManager.java
@@ -0,0 +1,99 @@
+package fr.enzias.easyduels.tasks;
+
+import fr.enzias.easyduels.EasyDuels;
+import fr.enzias.easyduels.arena.Arena;
+import fr.enzias.easyduels.arena.ArenaStatuts;
+import fr.enzias.easyduels.files.SettingsFile;
+import fr.enzias.easyduels.managers.CountdownManager;
+import fr.enzias.easyduels.queue.QueueManager;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class TimerManager extends BukkitRunnable {
+
+    private final EasyDuels plugin;
+    Arena arena;
+    SettingsFile settings;
+    QueueManager queue;
+    CountdownManager countdown;
+    int lobbyTime;
+    int fightTime;
+    int reloadTime;
+    public TimerManager(EasyDuels plugin) {
+        this.plugin = plugin;
+        this.arena = plugin.getArena();
+        this.settings = plugin.getSettingsFile();
+        this.queue = plugin.getQueue();
+        this.countdown = new CountdownManager(plugin);
+        this.lobbyTime = arena.getLobbyTime();
+        this.fightTime = arena.getPlayingTime();
+        this.reloadTime = arena.getReloadingTime();
+    }
+
+    @Override
+    public void run() {
+
+
+        if(arena.isStatut(ArenaStatuts.LOBBY)) {
+
+            countdown.sendLobbyTimer(lobbyTime);
+
+            if (lobbyTime <= 0) {
+                arena.setStatut(ArenaStatuts.PLAYING);
+            }
+            lobbyTime--;
+        }
+        if(arena.isStatut(ArenaStatuts.PLAYING)){
+
+            countdown.sendFightTime(fightTime);
+
+            if(fightTime <= 0){
+                arena.setStatut(ArenaStatuts.RELOADING);
+            }
+
+            fightTime--;
+
+        }
+        if(arena.isStatut(ArenaStatuts.RELOADING)){
+
+            countdown.sendEndTime(reloadTime);
+
+            if(reloadTime <= 0) {
+
+                if(settings.getSyncTimer()){
+                    if(arena.getFirstPlayer().isOnline()
+                            && arena.getPlayers().contains(arena.getFirstPlayer())) {
+                        arena.teleportToLastLocation(arena.getFirstPlayer());
+                    }
+                    if(arena.getSecondPlayer().isOnline()
+                            && arena.getPlayers().contains(arena.getFirstPlayer())) {
+                        arena.teleportToLastLocation(arena.getSecondPlayer());
+                    }
+                }
+                else {
+                    Bukkit.getScheduler().runTask(plugin, () -> {
+                        if(arena.getFirstPlayer().isOnline()
+                                && arena.getPlayers().contains(arena.getFirstPlayer())) {
+                            arena.teleportToLastLocation(arena.getFirstPlayer());
+                        }
+                        if(arena.getSecondPlayer().isOnline()
+                                && arena.getPlayers().contains(arena.getFirstPlayer())) {
+                            arena.teleportToLastLocation(arena.getSecondPlayer());
+                        }
+                    });
+                }
+
+                arena.resetArena();
+                arena.setStatut(ArenaStatuts.IDLE);
+                if(settings.getQueue())
+                    queue.checkQueue();
+                this.cancel();
+
+            }
+
+            reloadTime--;
+
+        }
+
+    }
+}
