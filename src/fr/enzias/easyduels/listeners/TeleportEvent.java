05597f35999f5605484b2e7917a2d2d40643c37b
Enzias <68203437+Enzias@users.noreply.github.com>
2020-09-23 11:54:49 +0200

    Added Spectate check

--- src/fr/enzias/easyduels/listeners/TeleportEvent.java
+++ src/fr/enzias/easyduels/listeners/TeleportEvent.java
@@ -3,6 +3,7 @@ package fr.enzias.easyduels.listeners;
 import fr.enzias.easyduels.EasyDuels;
 import fr.enzias.easyduels.arena.Arena;
 import fr.enzias.easyduels.arena.ArenaStatuts;
+import fr.enzias.easyduels.arena.Spectate;
 import fr.enzias.easyduels.files.ArenaFile;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,15 +15,27 @@ public class TeleportEvent implements Listener {
     private final EasyDuels plugin;
     ArenaFile arenaFile;
     Arena arena;
+    Spectate spectate;
 
     public TeleportEvent(EasyDuels plugin) {
         this.plugin = plugin;
         this.arenaFile = plugin.getArenaFile();
         this.arena = plugin.getArena();
+        this.spectate = plugin.getSpectate();
     }
 
     @EventHandler
     public void onTeleport(PlayerTeleportEvent event){
+
+        if (!arena.isStatut(ArenaStatuts.IDLE)) {
+
+            Player player = event.getPlayer();
+
+            if (player.getLocation().getWorld().getName().equalsIgnoreCase(arenaFile.getWorldName())
+                    && spectate.isSpectating(player))
+                spectate.finishSpectating(player);
+        }
+
         if (arena.isStatut(ArenaStatuts.PLAYING) || arena.isStatut(ArenaStatuts.LOBBY))
             if(event.getCause() != PlayerTeleportEvent.TeleportCause.PLUGIN) {
 
