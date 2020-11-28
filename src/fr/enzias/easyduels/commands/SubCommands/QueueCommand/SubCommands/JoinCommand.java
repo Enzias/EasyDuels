67ec7d470094384e020f3f6e06e8851cd42d6002
Enzias <68203437+Enzias@users.noreply.github.com>
2020-11-28 12:43:36 +0100

    Update JoinCommand.java

--- src/fr/enzias/easyduels/commands/SubCommands/QueueCommand/SubCommands/JoinCommand.java
+++ src/fr/enzias/easyduels/commands/SubCommands/QueueCommand/SubCommands/JoinCommand.java
@@ -1,29 +1,14 @@
-package fr.enzias.easyduels.commands.SubCommands.QueueCommand.SubCommands;
+package fr.enzias.easyduels.commands.subcommands.queuecommand.subcommands;
 
 import fr.enzias.easyduels.EasyDuels;
-import fr.enzias.easyduels.arena.Arena;
 import fr.enzias.easyduels.arena.ArenaStatuts;
 import fr.enzias.easyduels.commands.SubCommand;
-import fr.enzias.easyduels.files.ArenaFile;
-import fr.enzias.easyduels.files.MessageFile;
-import fr.enzias.easyduels.managers.SenderManager;
-import fr.enzias.easyduels.queue.QueueManager;
 import org.bukkit.entity.Player;
 
 public class JoinCommand extends SubCommand {
 
-    Arena arena;
-    ArenaFile arenaFile;
-    MessageFile messageFile;
-    QueueManager queue;
-    SenderManager sender;
     public JoinCommand(EasyDuels plugin) {
         super(plugin);
-        this.arena = plugin.getArena();
-        this.arenaFile = plugin.getArenaFile();
-        this.messageFile = plugin.getMessageFile();
-        this.queue = plugin.getQueue();
-        this.sender = plugin.getSender();
     }
 
     @Override
