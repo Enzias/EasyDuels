3021c8763d650c9b85163fc2a0ba0528d243fa0a
Enzias <68203437+Enzias@users.noreply.github.com>
2020-11-28 12:39:32 +0100

    Update LeaveCommand.java

--- src/fr/enzias/easyduels/commands/SubCommands/SpectateCommand/SubCommands/LeaveCommand.java
+++ src/fr/enzias/easyduels/commands/SubCommands/SpectateCommand/SubCommands/LeaveCommand.java
@@ -1,28 +1,13 @@
-package fr.enzias.easyduels.commands.SubCommands.SpectateCommand.SubCommands;
+package fr.enzias.easyduels.commands.subcommands.spectatecommand.subcommands;
 
 import fr.enzias.easyduels.EasyDuels;
-import fr.enzias.easyduels.arena.Arena;
-import fr.enzias.easyduels.arena.Spectate;
 import fr.enzias.easyduels.commands.SubCommand;
-import fr.enzias.easyduels.files.ArenaFile;
-import fr.enzias.easyduels.files.MessageFile;
-import fr.enzias.easyduels.managers.SenderManager;
 import org.bukkit.entity.Player;
 
 public class LeaveCommand extends SubCommand {
 
-    Arena arena;
-    Spectate spectate;
-    ArenaFile arenaFile;
-    MessageFile messageFile;
-    SenderManager sender;
     public LeaveCommand(EasyDuels plugin) {
         super(plugin);
-        this.arena = plugin.getArena();
-        this.spectate = plugin.getSpectate();
-        this.arenaFile = plugin.getArenaFile();
-        this.messageFile = plugin.getMessageFile();
-        this.sender = plugin.getSender();
     }
 
     @Override
