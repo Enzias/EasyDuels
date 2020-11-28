2c37d965601a08972660c9ce6bdcd254e6c6dc1f
Enzias <68203437+Enzias@users.noreply.github.com>
2020-11-28 12:45:25 +0100

    Update SecondSpawnCommand.java

--- src/fr/enzias/easyduels/commands/SubCommands/AdminCommand/SubCommands/SecondSpawnCommand.java
+++ src/fr/enzias/easyduels/commands/SubCommands/AdminCommand/SubCommands/SecondSpawnCommand.java
@@ -1,22 +1,13 @@
-package fr.enzias.easyduels.commands.SubCommands.AdminCommand.SubCommands;
+package fr.enzias.easyduels.commands.subcommands.admincommand.subcommands;
 
 import fr.enzias.easyduels.EasyDuels;
 import fr.enzias.easyduels.commands.SubCommand;
-import fr.enzias.easyduels.files.ArenaFile;
-import fr.enzias.easyduels.files.MessageFile;
-import fr.enzias.easyduels.managers.SenderManager;
 import org.bukkit.entity.Player;
 
 public class SecondSpawnCommand extends SubCommand {
 
-    ArenaFile arenaFile;
-    MessageFile messageFile;
-    SenderManager sender;
     public SecondSpawnCommand(EasyDuels plugin) {
         super(plugin);
-        this.arenaFile = plugin.getArenaFile();
-        this.messageFile = plugin.getMessageFile();
-        this.sender = plugin.getSender();
     }
 
     @Override
